5655cb32963410fbee3b92a04f4cbd9e9a653f0f
OlivierArthur <163600465+OlivierArthur@users.noreply.github.com>
2026-03-27 15:21:02 +0100

    Zmiana podejścia do pobierania modelu

--- kod_zrodlowy/kod.py
+++ kod_zrodlowy/kod.py
@@ -1,153 +1,153 @@
-import os
-import pandas as pd
-import torch
 import numpy as np
-import scipy.special
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import accuracy_score, precision_recall_fscore_support
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer, TrainingArguments, EvalPrediction
-import mlflow
-import dagshub
+import torch
+import evaluate
+from datasets import load_dataset
+from transformers import (
+    AutoTokenizer,
+    AutoModelForSequenceClassification,
+    TrainingArguments,
+    Trainer,
+    DataCollatorWithPadding,
+    EarlyStoppingCallback
+)
+from huggingface_hub import login
+from huggingface_hub import notebook_login
+notebook_login()
 
-dagshub.init(repo_owner='OlivierArthur', repo_name='BERT_porownanie_treningowych', mlflow=True)
-mlflow.set_experiment("BERT_Spam_porown_zb_treng")
 
-#-o to nadpisane plikow
-os.system("kaggle datasets download -d nitishabharathi/email-spam-dataset --unzip -o")
+#kolumny muszą być text albo label
 
-datasety = [
-    {"nazwa": "Exp_1_SpamAssassin", "csv_nazwa": "completeSpamAssassin.csv", "text": "Body", "label": "Label"},
-    {"nazwa": "Exp_2_Enron", "csv_nazwa": "enronSpamSubset.csv", "text": "Body", "label": "Label"},
-    {"nazwa": "Exp_3_LingSpam", "csv_nazwa": "lingSpam.csv", "text": "Body", "label": "Label"}
-]
+def obrabianie_zbioru(ds):
+    col_names = ds.column_names
 
-#klasa konwertująca surowe tokeny i etykiety na tensory wymagane przez bibliotekę PyTorch
-class SpamDataset(torch.utils.data.Dataset):
-    def __init__(self, encodings, labels):
-        self.encodings = encodings
-        self.labels = labels
+    text_col = next((col for col in col_names if col.lower() in ["text", "email text", "message", "body", "content", "email"]), None)
+    label_col = next((col for col in col_names if col.lower() in ["label", "labels", "is_spam", "spam", "target", "class", "email type"]), None)
 
-    def __getitem__(self, idx):
-        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
-        item['labels'] = torch.tensor(self.labels[idx])
-        return item
+    if text_col and text_col != "text":
+        ds = ds.rename_column(text_col, "text")
 
-    def __len__(self):
-        return len(self.labels)
+    if label_col and label_col != "label":
+        ds = ds.rename_column(label_col, "label")
 
-#inicjalizacja tokenizatora z pre-trenowanego modelu BERT
+    def format_labels(example):
+        val = str(example["label"]).lower().strip()
+        if val in ["1", "phishing email", "spam", "phishing", "true"]:
+            return {"label": 1}
+        else:
+            return {"label": 0}
+
+    ds = ds.map(format_labels)
+    return ds.select_columns(["text", "label"])
+
+
+
+train_data = load_dataset("puyang2025/seven-phishing-email-datasets", split="train")
+train_data = obrabianie_zbioru(train_data).shuffle(seed=42)
+
+
+test_data = load_dataset("zefang-liu/phishing-email-dataset", split="train")
+test_data = obrabianie_zbioru(test_data)
+
+print(f"Trenowanie {len(train_data)}")
+print(f"Testowanie {len(test_data)}")
+
+train_data = train_data.filter(lambda x: x["text"] is not None and str(x["text"]).strip() != "")
+test_data = test_data.filter(lambda x: x["text"] is not None and str(x["text"]).strip() != "")
+
+train_data = train_data.map(lambda x: {"text": str(x["text"])})
+test_data = test_data.map(lambda x: {"text": str(x["text"])})
+
+#tokenizacja
 tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
 
-def compute_metrics(p: EvalPrediction):
-    logits = p.predictions
-    labels = p.label_ids
-    preds = np.argmax(logits, axis=1)
-    acc = accuracy_score(labels, preds)
-    precision, recall, f1, _ = precision_recall_fscore_support(labels, preds, average='binary')
+def preprocess_function(examples):
+    return tokenizer(examples["text"], truncation=True, padding=True, max_length=512)
+
+tokenized_train = train_data.map(preprocess_function, batched=True, remove_columns=["text"])
+tokenized_test = test_data.map(preprocess_function, batched=True, remove_columns=["text"])
+
+
+f1_metric = evaluate.load("f1")
+precision_metric = evaluate.load("precision")
+recall_metric = evaluate.load("recall")
+
+def compute_metrics(eval_pred):
+    logits, labels = eval_pred
+    predictions = np.argmax(logits, axis=-1)
 
     return {
-        'accuracy': acc,
-        'f1': f1,
-        'precision': precision,
-        'recall': recall
+        "f1": f1_metric.compute(predictions=predictions, references=labels)["f1"],
+        "precision": precision_metric.compute(predictions=predictions, references=labels)["precision"],
+        "recall": recall_metric.compute(predictions=predictions, references=labels)["recall"],
     }
 
 
-for data in datasety:
-    print(f"\n START EKSPERYMENTU: {data['nazwa']}")
+id2label = {0: "ham", 1: "spam"}
+label2id = {"ham": 0, "spam": 1}
 
-    #wczytanie
-    df = pd.read_csv(data["csv_nazwa"], encoding='utf-8', encoding_errors='replace')
-
-    #kolumny i drop na
-    df = df[[data['text'], data['label']]].dropna()
-
-    df = df.drop_duplicates(subset=[data['text']], keep='first') #usuwamy duplikaty
-
-    #ograniczenie zbioru do max 2000 losowych próbek
-    #df = df.sample(n=min(2000, len(df)), random_state=42)
-
-    #lebel encoder do wartosci binarnych
-    le = LabelEncoder()
-    labels = le.fit_transform(df[data['label']])
-    texts = df[data['text']].astype(str).tolist()
-
-    #podział na zbiór treningowy i walidacyjny 4 do 1
-    #train_texts, val_texts, train_labels, val_labels = train_test_split(texts, labels, test_size=0.2, random_state=42)
-
-    #stratified sampling pomoże z imbalansem danych (np. Lingspam mógł się uczyć na samym hamie w teorii)
-
-    train_texts, val_texts, train_labels, val_labels = train_test_split(
-    texts,
-    labels,
-    test_size=0.2,
-    random_state=42,
-    stratify=labels
+model = AutoModelForSequenceClassification.from_pretrained(
+    "bert-base-uncased",
+    num_labels=2,
+    id2label=id2label,
+    label2id=label2id
 )
 
-    #tokenizacja
-    train_encodings = tokenizer(train_texts, truncation=True, padding=True, max_length=512)
-    val_encodings = tokenizer(val_texts, truncation=True, padding=True, max_length=512)
+for param in model.bert.encoder.layer[:6].parameters():
+    param.requires_grad = False
 
-    #Pytorch wymaga takiego czegoś
-    train_dataset = SpamDataset(train_encodings, train_labels)
-    val_dataset = SpamDataset(val_encodings, val_labels)
-
-
-    with mlflow.start_run(run_name=(data['nazwa'])+' stratified, zamrożone 3, pełne dane'):
-
-        #pobranie berta i dodanie warstwy do klasyfikacji z 2 labels - 2 klasy
-        model = AutoModelForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=2) #Mozna usunac '#' poniżej żeby trenować tylko warstwę klasyfikacyjną, wypada do tego zwiększyć learning_rate do 2e-3
-        #for param in model.bert.parameters():
-          #param.requires_grad = False
-
-        #zamrażamy pierwszą warstwę
-        for param in model.bert.embeddings.parameters():
-            param.requires_grad = False
-
-        #Zamrażamy pierwsze 3 warstwy, zmienną można zmieniać
-        warstwy_do_zamrozenia = 3
-        for i in range(warstwy_do_zamrozenia):
-            for param in model.bert.encoder.layer[i].parameters():
-                param.requires_grad = False
-
-
-        training_args = TrainingArguments(
-            output_dir=f"./wyniki_{data['nazwa']}",
-            num_train_epochs=3,
-            per_device_train_batch_size=8,
-            per_device_eval_batch_size=8,
-            eval_strategy="epoch",
-            save_strategy="epoch",
-            learning_rate=2e-5,
-            report_to="mlflow",
-            logging_steps=10,
-            weight_decay=0.01,
-            save_total_limit=1,
-            warmup_ratio=0.1
-        )
+#Musimy obniżyć batch size do 8 przy max_length=512
+#chcemy żeby nasz model miał możliwie najlepsze f1
+#5 epok to zwykle za dużo jak na bert, mamy early stopping
+training_args = TrainingArguments(
+    output_dir="./klasyfikatorspamu1",
+    learning_rate=2e-5,
+    per_device_train_batch_size=8,
+    per_device_eval_batch_size=16,
+    gradient_accumulation_steps=4,
+    num_train_epochs=5,
+    weight_decay=0.05,
+    warmup_ratio=0.1,
+    eval_strategy="steps",
+    eval_steps=500,
+    save_strategy="steps",
+    save_steps=500,
+    load_best_model_at_end=True,
+    metric_for_best_model="f1",
+    greater_is_better=True,
+    fp16=torch.cuda.is_available(),
+    logging_dir='./logs',
+    logging_steps=100,
+    push_to_hub=True,
+    hub_model_id="OliverArt5500/klasyfikatorspamu1",
+    hub_strategy="end",
+)
 
 
-        trainer = Trainer(
-            model=model,
-            args=training_args,
-            train_dataset=train_dataset,
-            eval_dataset=val_dataset,
-            compute_metrics=compute_metrics
-        )
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
-        print(f"trening na danych {data['csv_nazwa']}...")
-        trainer.train()
+#wspomniany early stopping
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_train,
+    eval_dataset=tokenized_test,
+    processing_class=tokenizer,
+    data_collator=data_collator,
+    compute_metrics=compute_metrics,
+    callbacks=[EarlyStoppingCallback(early_stopping_patience=3)]
+)
 
-        trainer.evaluate()
 
-        del model
-        del trainer
-        import gc
-        gc.collect() #Wymuszenie odśmiecacza pamięci
-        torch.cuda.empty_cache()
+print("Trwa trenowanie: ")
+trainer.train()
 
-        print(f" Zakończono: {data['nazwa']}")
+print("\n testowanie: ")
+metrics = trainer.evaluate()
+for key, value in metrics.items():
+    print(f"{key}: {value:.4f}")
 
-print("\n KONIEC")
+
+trainer.save_model("./klasyfikatorspamu1")
+trainer.push_to_hub(commit_message=" Aktualizacja modelu klasyfikatorspamu1")
+
+print("koniec")
